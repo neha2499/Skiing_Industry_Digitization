743a25a157b45b718aad408f1e6a92e4dfef7eb7
neha2499 <chaudharyneha2499@gmail.com>
2023-02-03 22:46:28 -0500

    first commit

---
+++ src/main/java/com/coen6731/group8/Group8Application.java
@@ -0,0 +1,13 @@
+package com.coen6731.group8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coen6731/group8/Group8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coen6731.group8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
