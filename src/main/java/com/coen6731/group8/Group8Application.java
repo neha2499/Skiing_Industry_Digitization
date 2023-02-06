54a8a61295aa61ac26ee2f2cb0782544e1a177a0
neha2499 <chaudharyneha2499@gmail.com>
2023-02-06 10:17:53 -0500

    new changes

--- src/main/java/com/coen6731/group8/Group8Application.java
+++ src/main/java/com/coen6731/group8/Group8Application.java
@@ -1,10 +1,20 @@
 package com.coen6731.group8;
 
+import com.coen6731.group8.repository.ResortRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+
+@EnableMongoRepositories(basePackageClasses = ResortRepository.class)
 @SpringBootApplication
 public class Group8Application {
+
+
+
+	@Autowired
+	ResortRepository resortRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group8Application.class, args);
--- src/main/java/com/coen6731/group8/resort/ResortDaoService.java
+++ src/main/java/com/coen6731/group8/resort/ResortDaoService.java
@@ -1,4 +1,20 @@
 package com.coen6731.group8.resort;
 
+import com.coen6731.group8.repository.ResortRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ResortDaoService {
+
+
+
+    @Autowired
+    private ResortRepository resortRepository;
+
+
+
+    public Resort createResort(Resort resort){return resortRepository.save(resort);}
+
 }
