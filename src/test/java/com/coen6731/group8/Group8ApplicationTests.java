758a4281b253862c8814f41cd3e8cc44cb35beb4
AditShah1234 <48185533+AditShah1234@users.noreply.github.com>
2023-02-28 19:44:19 -0500

    threaded client

--- src/test/java/com/coen6731/group8/Group8ApplicationTests.java
+++ src/test/java/com/coen6731/group8/Group8ApplicationTests.java
@@ -8,6 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SpringBootTest
 class Group8ApplicationTests {
@@ -17,14 +22,128 @@ class Group8ApplicationTests {
 
 		Body body = new Body(skier.getTime(),skier.getLiftID());
 		WebClient webClient = WebClient.create("http://localhost:8083");
-		String responseBody = webClient.post()
-				.uri("/skiers/"+String.valueOf(skier.getSkierID())+"/seasons/"+String.valueOf(skier.getSeasonID())+"/days/"+String.valueOf(skier.getDayID())+"/skiers/"+String.valueOf(skier.getSkierID()))
+		String responseBody = webClient.post().uri("/skiers/"+String.valueOf(skier.getSkierID())+"/seasons/"+String.valueOf(skier.getSeasonID())+"/days/"+String.valueOf(skier.getDayID())+"/skiers/"+String.valueOf(skier.getSkierID()))
 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 				.body(Mono.just(body), Body.class)
 				.retrieve()
 				.bodyToMono(String.class)
 				.block();
 
+
 		System.out.println(responseBody);
+	}
+
+
+	@Test
+	void threaded_clients() throws Exception {
+
+		Instant start_now, end_now;
+		Duration timeElapsed;
+		Skier skier = new Skier();
+
+		Body body = new Body(skier.getTime(),skier.getLiftID());
+		WebClient webClient = WebClient.create("http://localhost:8083");
+		String responseBody = webClient.post().uri("/skiers/"+String.valueOf(skier.getSkierID())+"/seasons/"+String.valueOf(skier.getSeasonID())+"/days/"+String.valueOf(skier.getDayID())+"/skiers/"+String.valueOf(skier.getSkierID()))
+				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.body(Mono.just(body), Body.class)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+
+		System.out.println(responseBody);
+		String url = "http://localhost:8083";
+
+		Integer total =0;
+		Integer no_clients =32;
+		Integer no_post  = 10 ;
+		List<thread_client> clients = new ArrayList<>();
+
+		//////////////////////////////////////////////////////
+
+		start_now = Instant.now();
+
+		for (int i = 0; i < no_clients; i++) {
+			thread_client R1 = new thread_client(Integer.toString(i), url, no_post);
+			R1.start();
+			clients.add(R1);
+		}
+
+//		for (int i = 0; i < no_clients; i++) {
+//			try {
+//				clients.get(i).join();
+//			} catch (InterruptedException e) {
+//				System.out.println("Thread interrupted");
+//			}
+//		}
+
+		int k =0,flag=0;
+		while (k<1000000) {
+			for (int i = 0; i < no_clients; i++) {
+				Thread t = clients.get(i).getT();
+				if (t.isAlive()) {
+					try {
+						t.join();
+					} catch (InterruptedException e) {
+						// ignore
+					}
+				} else {
+					// interrupt all other threads and remove them from the list
+					for (int j = 0; j < no_clients; j++) {
+						Thread t2 = clients.get(j).getT();
+						if (t2 != t) {
+							t2.interrupt();
+						}
+					}
+					flag =1;
+
+				}
+			}
+			if (flag ==1)
+				break;
+
+		}
+		for (int j = 0; j < no_clients; j++) {
+			total += clients.get(j).getCount();
+		}
+
+		no_clients =100;
+		int i1 = (1000 - total) / no_clients;
+		System.out.println(String.valueOf(i1)+"fjhgfhfyh"+String.valueOf(total));
+		no_post =  i1;
+		for (int i = 0; i < no_clients; i++) {
+			if (i > 31){
+				thread_client R1 = new thread_client(Integer.toString(i), url, no_post);
+				R1.start();
+				clients.add(R1);
+				}
+			else
+				clients.get(i).start();
+		}
+
+		for (int i = 0; i < no_clients; i++) {
+			try {
+				clients.get(i).join();
+			} catch (InterruptedException e) {
+				System.out.println("Thread interrupted"+e);
+			}
+		}
+
+
+
+		end_now = Instant.now();
+
+		//////////////////////////////////////////////////////
+
+		timeElapsed = Duration.between(start_now, end_now);
+
+
+		System.out.println(timeElapsed);
+//		ArrayList<Integer> time_dif = new ArrayList<>();
+//		for (int i = 0; i < no_clients; i++) {
+////			System.out.println(clients.get(i).getTimeElapsed().toMillisPart());
+//			time_dif.add(clients.get(i).getTimeElapsed().toMillisPart());
+//		}
+
 	}
 }
---
+++ src/test/java/com/coen6731/group8/thread_client.java
@@ -0,0 +1,154 @@
+package com.coen6731.group8;
+
+
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+
+
+public class thread_client implements Runnable {
+    private Thread t;
+    private String threadName;
+    private String url;
+    int n;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    int count;
+    private Instant start_now,end_now;
+    private Duration timeElapsed;
+
+    public ArrayList<Duration> getTime_post() {
+        return time_post;
+    }
+
+
+
+    private ArrayList<Duration> time_post;
+
+    thread_client( String name, String url, Integer n) {
+        threadName = name;
+        this.url =url;
+        this.n =n;
+        System.out.println("Creating " +  threadName );
+    }
+
+    public void run() {
+        System.out.println("Running " +  threadName );
+
+
+        try {
+
+            for (int i = 0; i < n; i++) {
+                start_now = Instant.now();
+
+                Skier skier = new Skier();
+
+                Body body = new Body(skier.getTime(), skier.getLiftID());
+                WebClient webClient = WebClient.create(url);
+                try {
+                    String responseBody = webClient.post().uri("/skiers/" + String.valueOf(skier.getSkierID()) + "/seasons/" + String.valueOf(skier.getSeasonID()) + "/days/" + String.valueOf(skier.getDayID()) + "/skiers/" + String.valueOf(skier.getSkierID()))
+                            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                            .body(Mono.just(body), Body.class)
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .block();
+                    count+=1;
+                }catch(Exception e) {
+                    for (int j=0;j<4;j++){
+                        try {
+                            String responseBody = webClient.post().uri("/skiers/" + String.valueOf(skier.getSkierID()) + "/seasons/" + String.valueOf(skier.getSeasonID()) + "/days/" + String.valueOf(skier.getDayID()) + "/skiers/" + String.valueOf(skier.getSkierID()))
+                                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                    .body(Mono.just(body), Body.class)
+                                    .retrieve()
+                                    .bodyToMono(String.class)
+                                    .block();
+                            break;
+                        } catch (Exception ex) {
+                            if (j >= 3)
+                                throw new RuntimeException(ex);
+                        }
+
+                    }
+                }
+
+                end_now = Instant.now();
+                timeElapsed = Duration.between(start_now, end_now);
+                time_post.add(timeElapsed);
+
+            }
+
+
+
+
+                timeElapsed = timeElapsed.dividedBy(n);
+                System.out.println("Creating " + threadName + "executed success");
+            }catch(Exception e){
+                System.out.println("Thread " + threadName + " interrupted.");
+            }
+        }
+
+
+
+    public void start () {
+        System.out.println("Starting " +  threadName );
+        if (t == null) {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+    public void join() throws InterruptedException {
+        t.join ();
+    }
+
+    public Duration getTimeElapsed() {
+        return timeElapsed;
+    }
+
+    public void setTimeElapsed(Duration timeElapsed) {
+        this.timeElapsed = timeElapsed;
+    }
+
+    public Thread getT() {
+        return t;
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public Instant getStart_now() {
+        return start_now;
+    }
+
+    public Instant getEnd_now() {
+        return end_now;
+    }
+
+
+
+}
