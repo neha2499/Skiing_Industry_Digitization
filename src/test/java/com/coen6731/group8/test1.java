1bba2774167dd17af46e07774a0e027b89d9a87d
AditShah1234 <48185533+AditShah1234@users.noreply.github.com>
2023-03-02 12:11:35 -0500

    test1 added for seq test

--- src/test/java/com/coen6731/group8/test1.java
+++ src/test/java/com/coen6731/group8/test1.java
@@ -4,23 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 
 
 @SpringBootTest
-class Group8ApplicationTests {
+class test1 {
 	String url = "http://155.248.226.228:8083";
 
 
@@ -60,96 +57,34 @@ class Group8ApplicationTests {
 
 
 	@Test
-	void threaded_clients() throws Exception {
+	void clients() throws Exception {
 
 		Instant start_now, end_now;
 		Duration timeElapsed;
-		Skier skier = new Skier();
-		int total_client = 100;
+		int total_client = 1;
 		int desired_no_post = 100000;
-
-
-
-
-
-
-		Integer no_clients =32;
-		Integer no_post  = 1000 ;
-		List<thread_client> clients = new ArrayList<>();
-
-		//////////////////////////////////////////////////////
-
 		start_now = Instant.now();
-
-		for (int i = 0; i < no_clients; i++) {
-			thread_client R1 = new thread_client(Integer.toString(i), url, no_post);
-			R1.start();
-			clients.add(R1);
-		}
-
-		int flag=0;
-		while (true) {
-			for (int i = 0; i < no_clients; i++) {
-				Thread t = clients.get(i).getT();
-				if (t.isAlive()) continue;
-				else{
-					flag =1;
-				}
-			}
-			if (flag ==1)
-				break;
-		}
-
-
-
-		int i1 = (desired_no_post - (no_clients*no_post)) / (total_client-no_clients);
-		System.out.println(String.valueOf(i1)+"<><><><><><><><><><><><><><><><><><><><><>");
-
-		for (int i = no_clients; i < total_client; i++) {
-
-			thread_client R1 = new thread_client(Integer.toString(i), url, i1);
-			R1.start();
-			clients.add(R1);
-		}
-
-		for (int i = 0; i < total_client; i++) {
-			try {
-				clients.get(i).join();
-			} catch (InterruptedException e) {
-				System.out.println("Thread interrupted"+e);
-			}
-		}
-
-
-
+		thread_client R1 = new thread_client(Integer.toString(total_client), url, desired_no_post);
+		R1.start();
+		R1.join();
 		end_now = Instant.now();
-
-		//////////////////////////////////////////////////////
-
 		timeElapsed = Duration.between(start_now, end_now);
 
 
 		System.out.println(timeElapsed);
 		ArrayList<Integer> time_dif = new ArrayList<>();
-		String fileName = "output.csv";
+		String fileName = "output_seq.csv";
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-			for (int i = 0; i < total_client; i++) {
-				System.out.println(clients.get(i).getTime_post().toString());
-				ArrayList<String> myList = clients.get(i).getTime_post();
 
 
-
-					for (String str : myList) {
-						writer.write(str);
-						writer.newLine();
-
-
-	//			time_dif.add(clients.get(i).getTimeElapsed().toMillisPart());
-						}
+			ArrayList<String> myList = R1.getTime_post();
+			for (String str : myList) {
+				writer.write(str);
+				writer.newLine();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
+
 }
